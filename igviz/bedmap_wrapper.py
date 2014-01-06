be96a4cf267c948eca352c80f70464b7f859ba5c
dfernan <dfernan@gmail.com>
2014-01-06 11:44:09 -0500

    more changes

--- igviz/bedmap_wrapper.py
+++ igviz/bedmap_wrapper.py
@@ -9,7 +9,8 @@ import myos
 import doe_reader
 import execs_commands 
 
-def run_bedmap(in_reference_fn, doe_csv, header_name_of_exp_id, in_map_dir, extension, bedmap_options, out_dir, logs_dir):
+def run_bedmap(in_reference_fn, doe_csv, header_name_of_exp_id, in_map_dir, extension, bedmap_options, out_dir, logs_dir, execute):
+    myos.check_if_directory_exists_create_it(out_dir)
     qname = 'regevlab'
     mem_usage = '5000'
     bed_map_fn_dict = doe_reader.create_experiment_fns(doe_csv, header_name_of_exp_id, in_map_dir, extension)
@@ -19,21 +20,23 @@ def run_bedmap(in_reference_fn, doe_csv, header_name_of_exp_id, in_map_dir, exte
         runcmd = execs_commands.bedops().bedmap(bedmap_options, in_reference_fn, bed_map_fn, out_fn)
         fullcmd = bsubcmd+'\"'+runcmd+'\"'
         print fullcmd
-        os.system(fullcmd)
+        if execute:
+            os.system(fullcmd)
     return 0
 
 def main():
     parser = optparse.OptionParser("Wrapper for phasedBam2bed nimrod script that divides a bam into two pat/mat beds")
-    parser.add_option('-r', '--in_reference_fn', action = "store")
+    parser.add_option('-r', '--in_reference_fn', action = "store", help="in case of mapping reads to vcf, reference_fn = vcf_fn")
     parser.add_option('-d', '--doe_csv_fn', action = "store")
     parser.add_option('--header_name_of_exp_id', action = "store")
-    parser.add_option('-i', '--in_map_dir', action = "store")
+    parser.add_option('-i', '--in_map_dir', action = "store", help = "in case of mapping reads to vcf, map_dir is the directory that contains all bam2bed (sorted) files")
     parser.add_option('-e', '--extension_after_exp_id', action = "store")
     parser.add_option('--bedmap_options', help = "--bedmap_options='--faster --count --echo'", action = "store")
     parser.add_option('-o', '--out_dir')
     parser.add_option('-l', '--logs_dir', action = "store")
+    parser.add_option('-x', '--execute', action = "store_true", default=False)
     (options, args) = parser.parse_args()
-    run_bedmap(options.in_reference_fn, options.doe_csv_fn, options.header_name_of_exp_id, options.in_map_dir, options.extension_after_exp_id, options.bedmap_options, options.out_dir, options.logs_dir)
+    run_bedmap(options.in_reference_fn, options.doe_csv_fn, options.header_name_of_exp_id, options.in_map_dir, options.extension_after_exp_id, options.bedmap_options, options.out_dir, options.logs_dir, options.execute)
     return 0
 
 if __name__ == "__main__":
